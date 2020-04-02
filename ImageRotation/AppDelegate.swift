9a93b71009d4efdedf61c86005751df133c08429
charleshs <charlous167@gmail.com>
2020-04-02 16:35:09 +0800

    Created Xcode project and added .gitignore file.

---
+++ ImageRotation/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ImageRotation
+//
+//  Created by Charles Hsieh on 2020/4/2.
+//  Copyright © 2020 Charles Hsieh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ ImageRotation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ImageRotation
+//
+//  Created by Charles Hsieh on 2020/4/2.
+//  Copyright © 2020 Charles Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
